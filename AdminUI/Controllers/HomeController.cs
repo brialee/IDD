e2ebc56b70a6bc3268c4a9cbb80b0db21d75e63f
John Brusaw <jwbrusaw@gmail.com>
2020-04-13 07:42:22 -0700

    Added a "Provider ID" column to main table (#41)
    On the homepage, there now exists a "Provider ID" column in the main table.
    I made sure the column is sortable, and added a filter that allows Provider ID to be filtered.
    I also renamed "Prime" to "Client Prime" to avoid confusion
    
    Signed-off-by John Brusaw <jbrusaw@pdx.edu>

--- AdminUI/Controllers/HomeController.cs
+++ AdminUI/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace AdminUI.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index(string sortOrder, string pName, string cName, string dateFrom, string dateTo, string prime, string id)
+        public IActionResult Index(string sortOrder, string pName, string cName, string dateFrom, string dateTo, string prime, string id, string providerId)
         {
             var sheets = GetSheets();
 
@@ -38,31 +38,32 @@ namespace AdminUI.Controllers
                 model.PName = pName;
                 sheets = sheets.Where(t => t.ProviderName.ToLower().Contains(pName.ToLower()));
             }
+            if (!string.IsNullOrEmpty(providerId))
+            {
+                model.ProviderId = providerId;
+                sheets = sheets.Where(t => t.ProviderID.ToLower().Contains(providerId.ToLower()));
+            }
 
             if (!string.IsNullOrEmpty(cName))
             {
                 model.CName = cName;
                 sheets = sheets.Where(t => t.ClientName.ToLower().Contains(cName.ToLower()));
             }
-
+            if (!string.IsNullOrEmpty(prime))
+            {
+                model.Prime = prime;
+                sheets = sheets.Where(t => t.ClientPrime.ToLower().Contains(prime.ToLower()));
+            }
             if (!string.IsNullOrEmpty(dateFrom))
             {
                 model.DateFrom = dateFrom;
                 sheets = sheets.Where(t => t.Submitted >= DateTime.Parse(dateFrom));
             }
-
             if (!string.IsNullOrEmpty(dateTo))
             {
                 model.DateTo = dateTo;
                 sheets = sheets.Where(t => t.Submitted <= DateTime.Parse(dateTo));
             }
-
-            if (!string.IsNullOrEmpty(prime))
-            {
-                model.Prime = int.Parse(prime);
-                sheets = sheets.Where(t => t.ClientPrime == prime);
-            }
-
             if (!string.IsNullOrEmpty(id))
             {
                 model.Id = int.Parse(id);
@@ -107,6 +108,12 @@ namespace AdminUI.Controllers
                     break;
                 case "hours_desc":
                     sheets = sheets.OrderByDescending(t => t.Hours);
+                    break;
+                case "providerid":
+                    sheets = sheets.OrderBy(t => t.ProviderID);
+                    break;
+                case "providerid_desc":
+                    sheets = sheets.OrderByDescending(t => t.ProviderID);
                     break;
                 default:
                     sheets = sheets.OrderBy(t => t.TimesheetID);
@@ -176,7 +183,6 @@ namespace AdminUI.Controllers
                 catch (DbUpdateConcurrencyException)
                 {
 
-                    Console.WriteLine("There was an error");
                     if (!_context.Timesheet.Any(e => e.TimesheetID == id))
                     {
                         return NotFound();
