11d76e45e224439344c28b95c85856539536c520
David Post <post@pdx.edu>
2020-04-14 01:39:45 -0700

    Converted to Appserver namespace
    We changed from mvc_trial directory structure to Appserver, this
    puts the namespaces in line.
    
    Signed-off-by: David Post <post@pdx.edu>

--- Appserver/Startup.cs
+++ Appserver/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace mvc_trial
+namespace Appserver
 {
     public class Startup
     {
--- AppserverTest/AdminControllereTest.cs
+++ AppserverTest/AdminControllereTest.cs
@@ -1,7 +1,7 @@
 using NUnit.Framework;
 using System;
 using System.IO;
-using mvc_trial.Controllers;
+using Appserver.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Appserver.Tests
