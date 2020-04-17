865a65a9b2e2b6e71d09fd45267b6996a2cafcca
Keaton Kraiger <43203096+keatonkraiger@users.noreply.github.com>
2020-04-17 15:21:49 -0700

    Updated FileUpload component to be compatible with the AppServer. (#46)
    Updated the image upload component to post to a different view that is
    compatible with the form data.
    
    Signed-off-by: Keaton Kraiger <kkraiger@pdx.edu>

--- PWA/src/components/Timesheet/FileUploader.vue
+++ PWA/src/components/Timesheet/FileUploader.vue
@@ -29,8 +29,7 @@
       <div class="example-btn">
         <file-upload
           class="btn btn-primary"
-					post-action="https://clownedpineapple.azurewebsites.net/api/
-					HttpTrigger1?code=amZ51qGYCfuLgelHpVFn1oyB3M/iEwHpunUAaTc5JOo/z6QUHNKWkQ=="
+					post-action="https://iddappserver.azurewebsites.net/ImageUpload/DocAsForm"
           :multiple="true"
           :drop="true"
           :drop-directory="true"
